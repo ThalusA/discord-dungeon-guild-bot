643d2c72d248218a25c3277f07cedd9cde369c7f
ThalusA <15985204+ThalusA@users.noreply.github.com>
2022-12-04 03:46:01 +0000

    Use API key

--- src/google.ts
+++ src/google.ts
@@ -10,17 +10,15 @@ export default class Google {
     this.logged_in = false
   }
 
-  async login (client: Client): Promise<{ sheets: sheets_v4.Sheets, script: script_v1.Script }> {
+  login (client: Client): { sheets: sheets_v4.Sheets, script: script_v1.Script } {
     if (!this.logged_in) {
-      const auth = google.auth.fromAPIKey(client.env.GOOGLE_API_KEY)
-      await auth.authorize()
       this.sheets = google.sheets({
         version: 'v4',
-        auth
+        auth: client.env.GOOGLE_API_KEY
       })
       this.script = google.script({
         version: 'v1',
-        auth
+        auth: client.env.GOOGLE_API_KEY
       })
       this.logged_in = true
     }
@@ -31,7 +29,7 @@ export default class Google {
   }
 
   async shiftSpreadsheetValues (client: Client, sheetId: number, [startRowIndex, endRowIndex]: [number, number], [startColumnIndex, endColumnIndex]: [number, number]): Promise<void> {
-    await (await this.login(client)).sheets.spreadsheets.batchUpdate({
+    await this.login(client).sheets.spreadsheets.batchUpdate({
       spreadsheetId: client.env.GOOGLE_SPREADSHEET_ID,
       requestBody: {
         requests: [{
@@ -51,14 +49,14 @@ export default class Google {
   }
 
   async callAppsScript (client: Client): Promise<void> {
-    await (await this.login(client)).script.scripts.run({
+    await this.login(client).script.scripts.run({
       requestBody: { function: 'update' },
       scriptId: client.env.GOOGLE_SCRIPT_ID
     })
   }
 
   async gatherSpreadSheetValue (client: Client, range: string, majorDimension: string): Promise<any[][] | null | undefined> {
-    const response = await (await this.login(client)).sheets.spreadsheets.values.get({
+    const response = await this.login(client).sheets.spreadsheets.values.get({
       spreadsheetId: client.env.GOOGLE_SPREADSHEET_ID,
       range,
       majorDimension,
@@ -68,7 +66,7 @@ export default class Google {
   }
 
   async updateSpreadSheetValue (client: Client, range: string, majorDimension: string, values: string[][], clearRange: boolean): Promise<void> {
-    const { sheets } = await this.login(client)
+    const { sheets } = this.login(client)
     if (clearRange) {
       await sheets.spreadsheets.values.clear({
         spreadsheetId: client.env.GOOGLE_SPREADSHEET_ID,
